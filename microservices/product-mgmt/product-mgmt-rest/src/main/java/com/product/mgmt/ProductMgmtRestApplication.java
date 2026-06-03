8caad58577777cc361bd843fd44002b620c99078
subrat devnath <subratnath63@gmail.com>
2026-06-03 23:02:49 +0530

    mysql changes

--- microservices/product-mgmt/product-mgmt-rest/src/main/java/com/product/mgmt/ProductMgmtRestApplication.java
+++ microservices/product-mgmt/product-mgmt-rest/src/main/java/com/product/mgmt/ProductMgmtRestApplication.java
@@ -3,9 +3,13 @@ package com.product.mgmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {"com.product.mgmt", "com.security.config"})
 @EnableEurekaClient
+@EnableJpaRepositories(basePackages = {"com.product.mgmt.repository.dao"})
+@EntityScan(basePackages = {"com.product.mgmt.repository.entity"})
 public class ProductMgmtRestApplication {
 
     public static void main(String[] args) {
--- microservices/security/security-rest/src/main/java/com/security/SecurityRestApplication.java
+++ microservices/security/security-rest/src/main/java/com/security/SecurityRestApplication.java
@@ -3,10 +3,13 @@ package com.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {"com.security", "com.user.mgmt.client"})
 @EnableEurekaClient
+@EnableJpaRepositories(basePackages = {"com.security.repository.dao"})
+@EntityScan(basePackages = {"com.security.repository.entity"})
 public class SecurityRestApplication {
 
     public static void main(String[] args) {
